4a3d39a720101313dc3fb31fc46bf2a7adf971d6
wareez92 <wareezsowemimo@gmail.com>
2024-06-12 18:38:25 -0500

    users & products routes working

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 // import pg and create client
 
-const pg = requrie("pg");
-const client = new pg.client(
+const pg = require("pg");
+const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/synthstorm_db"
 );
 
@@ -13,28 +13,29 @@ const bcrypt = require("bcrypt");
 // createTables
 
 const createTables = async () => {
-  const SQL = ` DROP TABLE IF EXISTS users;
-                DROP TABLE IF EXISTS products;
-                DROP TABLE IF EXISTS orders;
-                DROP TABLE IF EXISTS favorites;
-                DROP TABLE IF EXISTS wishlist;
-                DROP TABLE IF EXISTS cart;
+  const SQL = ` DROP TABLE IF EXISTS users CASCADE;
+                DROP TABLE IF EXISTS products CASCADE;
+                DROP TABLE IF EXISTS orders CASCADE;
+                DROP TABLE IF EXISTS favorites CASCADE;
+                DROP TABLE IF EXISTS wishlist CASCADE;
+                DROP TABLE IF EXISTS cart CASCADE;
                     CREATE TABLE users(
                         id UUID PRIMARY KEY,
                         username VARCHAR(50) NOT NULL UNIQUE,
-                        password VARCHAR(50) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
                         isAdmin BOOLEAN DEFAULT FALSE,
                         isBanned BOOLEAN DEFAULT FALSE,
-                        address VARCHAR(255)
+                        address VARCHAR(255),
+                        phone VARCHAR(50)
                     );  
                     CREATE TABLE products(
                         id UUID PRIMARY KEY,
                         name VARCHAR (50) NOT NULL,
-                        price INTEGER NOT NULL,
+                        price VARCHAR(50) NOT NULL,
                         details VARCHAR(255),
                         quantity VARCHAR(50) NOT NULL,
-                        reviews INTEGER,  
-                        outOfStock BOOLEAN DEFAULT FALSE
+                        rating INTEGER, 
+                        review VARCHAR(50)
                     );
                     CREATE TABLE orders(
                         id UUID PRIMARY KEY,
@@ -45,13 +46,13 @@ const createTables = async () => {
                         id UUID PRIMARY KEY,
                         user_id UUID REFERENCES users(id) NOT NULL,
                         product_id UUID REFERENCES products(id) NOT NULL,
-                        CONSTRAINT user_id_product_id UNIQUE (user_id, product_id),
+                        CONSTRAINT user_id_product_id_favorite UNIQUE (user_id, product_id)
                     );
                     CREATE TABLE wishlist(
                         id UUID PRIMARY KEY,
                         user_id UUID REFERENCES users(id) NOT NULL,
                         product_id UUID REFERENCES products(id) NOT NULL,
-                        CONSTRAINT user_id_product_id UNIQUE (user_id, product_id)
+                        CONSTRAINT unique_user_product_wishlist UNIQUE (user_id, product_id)
                     );
                     CREATE TABLE cart(
                         id UUID PRIMARY KEY,
@@ -62,34 +63,184 @@ const createTables = async () => {
   await client.query(SQL);
 };
 
-// Create
+// CREATE
 
-const createUser = async () => {};
-const createProduct = async () => {};
-const createWishlist = async () => {};
-const createOrder = async () => {};
-const createFavorites = async () => {};
+// createUser
 
-// Read
+const createUser = async ({ username, password, address, phone }) => {
+  const SQL = ` INSERT INTO users (id, username, password, address, phone) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 5),
+    address,
+    phone,
+  ]);
+  return response.rows[0];
+};
 
-const fetchUsers = async () => {};
-const fetchProducts = async () => {};
-const fetchWishlist = async () => {};
-const fetchOrders = async () => {};
-const fetchFavorites = async () => {};
+// createProduct
 
-// Upadte
+const createProduct = async ({
+  name,
+  price,
+  details,
+  quantity,
+  rating,
+  review,
+}) => {
+  const SQL = ` INSERT INTO products (id, name, price, details, quantity, rating, review) VALUES ($1, $2, $3, $4, $5, $6, $7) `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    name,
+    price,
+    details,
+    quantity,
+    rating,
+    review,
+  ]);
+  return response.rows[0];
+};
 
-const banUser = async () => {};
-const authorizeUser = async () => {};
+// createWishlist
 
-// Delete
+const createWishlist = async ({ user_id, product_id }) => {
+  const SQL = ` INSERT INTO product (id, user_id, product_id) VALUES ($1, $2, $3) RETURNING *`;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  return response.rows[0];
+};
 
-const deleteUser = async () => {};
-const deleteProduct = async () => {};
-const deleteWishlist = async () => {};
-const deleteOrder = async () => {};
-const deleteFavorite = async () => {};
+// createOrder
+
+const createOrder = async ({ user_id, product_id }) => {
+  const SQL = ` INSERT INTO orders (id, user_id, product_id) VALUES ($1, $2, $3) RETURNING *`;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  return response.rows[0];
+};
+
+// createFavorite
+
+const createFavorite = async ({ user_id, product_id }) => {
+  const SQL = ` INSERT INTO favorite (user_id, product_id) VALUES ($1, $2, $3) RETURNING *`;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  return response.rows[0];
+};
+
+// READ
+
+// fetchUsers
+
+const fetchUsers = async () => {
+  const SQL = ` SELECT * FROM users`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+// fetchProducts
+
+const fetchProducts = async () => {
+  const SQL = ` SELECT * FROM products`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+// fetchWishlist
+
+const fetchWishlist = async () => {
+  const SQL = ` SELECT * FROM wishlist`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+// fetchOrders
+
+const fetchOrders = async () => {
+  const SQL = ` SELECT * FROM orders`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+// fetchFavorites
+
+const fetchFavorites = async () => {
+  const SQL = ` SELECT * FROM favorites`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+// UPDATE
+
+// banUser
+
+const banUser = async ({ id, username }) => {
+  const SQL = ` UPDATE users 
+                SET isBanned = true 
+                WHERE id = $1 
+                AND username = $2`;
+  const response = await client.query(SQL, [uuid.v4(), username]);
+  return response.rows;
+};
+
+// authorizeUser
+
+const authorizeUser = async ({ id, username }) => {
+  const SQL = ` UPDATE users 
+                SET isAdmin = true
+                WHERE id = $1
+                AND username`;
+  const response = await client.query(SQL, [uuid.v4(), username]);
+  return response.rows;
+};
+
+// DELETE
+
+// deleteUser
+
+const deleteUser = async ({ id, username }) => {
+  const SQL = ` DELETE FROM users 
+                WHERE id = $1
+                AND username = $2`;
+  await client.query(SQL, [uuid.v4(), username]);
+};
+
+// deleteProduct
+
+const deleteProduct = async ({ id, user_id }) => {
+  const SQL = ` DELETE FROM products 
+                  WHERE id = $1
+                  AND user_id = $2`;
+  await client.query(SQL, [uuid.v4(), user_id]);
+};
+
+// deleteFromWishlist
+
+const deleteFromWishlist = async ({ id, user_id, product_id }) => {
+  const SQL = ` DELETE FROM wishlist 
+                  WHERE product_id = $1
+                    AND user_id = $2
+                    AND product_id = $4`;
+  await client.query(SQL, [uuid.v4(), user_id, product_id]);
+};
+
+// deleteOrder
+
+const deleteOrder = async ({ id, user_id, product_id }) => {
+  const SQL = ` DELETE FROM orders
+                  WHERE id = $1 
+                  AND user_id = $2
+                  AND product_id = $3`;
+  await client.query(SQL, [uuid.v4(), user_id, product_id]);
+};
+
+// deleteFavorite
+
+const deleteFavorite = async ({ id, user_id, product_id }) => {
+  const SQL = ` DELETE FROM favorites
+                  WHERE id = $1
+                  AND user_id = $2
+                  AND product_id = $3`;
+  await client.query(SQL, [uuid.v4(), user_id, product_id]);
+};
 
 // module exports
 
@@ -99,7 +250,8 @@ module.exports = {
   createProduct,
   createWishlist,
   createOrder,
-  createFavorites,
+  createFavorite,
+  createTables,
   fetchUsers,
   fetchProducts,
   fetchWishlist,
@@ -109,7 +261,7 @@ module.exports = {
   authorizeUser,
   deleteUser,
   deleteProduct,
-  deleteWishlist,
+  deleteFromWishlist,
   deleteOrder,
   deleteFavorite,
 };
--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const {
   createProduct,
   createWishlist,
   createOrder,
-  createFavorites,
+  createFavorite,
+  createTables,
   fetchUsers,
   fetchProducts,
   fetchWishlist,
@@ -16,7 +17,7 @@ const {
   authorizeUser,
   deleteUser,
   deleteProduct,
-  deleteWishlist,
+  deleteFromWishlist,
   deleteOrder,
   deleteFavorite,
 } = require("./db");
@@ -32,7 +33,121 @@ app.use(express.json());
 
 // init function
 
-const init = async () => {};
+const init = async () => {
+  // connect to databases
+
+  console.log("connecting to database...");
+  await client.connect();
+  console.log("connected to the database");
+
+  // instantiate tables
+
+  await createTables();
+
+  // add dummy users
+
+  const [jill, robin, kelechi, Elyran] = await Promise.all([
+    createUser({
+      username: "jill",
+      password: "S3cr3t",
+      address: "555 West nomans str",
+      phone: "1(800)-222-2222",
+    }),
+    createUser({
+      username: "robin",
+      password: "S3cr3t",
+      address: "555 West nomans str",
+      phone: "1(800)-333-3333",
+    }),
+    createUser({
+      username: "kelechi",
+      password: "S3cr3t",
+      address: "555 West nomans str",
+      phone: "1(800)-444-4444",
+    }),
+    createUser({
+      username: "Elyran",
+      password: "S3cr3t",
+      address: "555 West nomans str",
+      phone: "1(800)-555-5555",
+    }),
+  ]);
+
+  // products
+
+  const [
+    microbrute,
+    virus,
+    micron,
+    microfreak,
+    monologue,
+    hydrasynth,
+    opsix,
+    mininova,
+  ] = await Promise.all([
+    createProduct({
+      name: "microbrute",
+      price: "$199.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "virus",
+      price: "2,929.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "micron",
+      price: "$299.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "microfreak",
+      price: "$349.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "monologue",
+      price: "$369.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "hydrasynth",
+      price: "$599.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "opsix",
+      price: "$749.99",
+      details: "",
+      quantity: "20",
+    }),
+    createProduct({
+      name: "mininova",
+      price: "$449.99",
+      details: "",
+      quantity: "20",
+    }),
+  ]);
+
+  console.log("o------ TABLES ------o");
+  console.log("---users---");
+  console.log(await fetchUsers());
+  console.log("---products---");
+  console.log(await fetchProducts());
+
+  // create a port and a listener
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
 
 // invoke init
 
@@ -42,25 +157,54 @@ init();
 
 // GET
 
-app.get("", async (req, res, next) => {});
-app.get("", async (req, res, next) => {});
-app.get("", async (req, res, next) => {});
-app.get("", async (req, res, next) => {});
-app.get("", async (req, res, next) => {});
+// fetchUsers
+
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch (error) {
+    next(error);
+  }
+});
+
+// fetchProducts
+
+app.get("/api/product", async (req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch (error) {
+    next(error);
+  }
+});
+
+// fetchFavorites
+
+app.get("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    const SQL = ` SELECT * FROM favorites WHERE id= $1
+                  AND user_id = $2 `;
+    const response = await client.query(SQL, [req.params.id, req.body.user_id]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/users/:id/wishlist", async (req, res, next) => {});
+app.get("/api/users/:id/cart", async (req, res, next) => {});
 
 // POST
 
-app.post("", async (req, res, next) => {});
-app.post("", async (req, res, next) => {});
-app.post("", async (req, res, next) => {});
-app.post("", async (req, res, next) => {});
-app.post("", async (req, res, next) => {});
-app.post("", async (req, res, next) => {});
+app.post("/api/", async (req, res, next) => {});
+app.post("/api/", async (req, res, next) => {});
+app.post("/api/", async (req, res, next) => {});
+app.post("/api/", async (req, res, next) => {});
+app.post("/api/", async (req, res, next) => {});
+app.post("/api/", async (req, res, next) => {});
 
 // DELETE
 
-app.delete("", async (req, res, next) => {});
-app.delete("", async (req, res, next) => {});
-app.delete("", async (req, res, next) => {});
-app.delete("", async (req, res, next) => {});
-app.delete("", async (req, res, next) => {});
+app.delete("/api/", async (req, res, next) => {});
+app.delete("/api/", async (req, res, next) => {});
+app.delete("/api/", async (req, res, next) => {});
+app.delete("/api/", async (req, res, next) => {});
+app.delete("/api/", async (req, res, next) => {});
